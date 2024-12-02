ce6ed2e90eb2b1b8759529285136e9bd08d5b44e
Josep Rueda <rueda_999@hotmail.com>
2024-12-02 12:40:46 +0100

    encoder and motor added

--- ROSArduinoBridge/encoder_driver.h
+++ ROSArduinoBridge/encoder_driver.h
@@ -1,19 +1,20 @@
-/* *************************************************************
-   Encoder driver function definitions - by James Nugen
-   ************************************************************ */
-   
-   
+/**************************************************************
+  Encoder driver function definitions - by James Nugen
+  ************************************************************ */
 #ifdef ARDUINO_ENC_COUNTER
+  #include <RotaryEncoder.h>
   //below can be changed, but should be PORTD pins; 
   //otherwise additional changes in the code are required
-  #define LEFT_ENC_PIN_A PD2  //pin 2
-  #define LEFT_ENC_PIN_B PD3  //pin 3
-  
-  //below can be changed, but should be PORTC pins
-  #define RIGHT_ENC_PIN_A PC4  //pin A4
-  #define RIGHT_ENC_PIN_B PC5   //pin A5
+  #define LEFT_ENC_PIN_A 2  //pin 2
+  #define LEFT_ENC_PIN_B 3  //pin 3
+
+  //below can be changed, but should be PORTD pins
+  #define RIGHT_ENC_PIN_A 18  //pin 18
+  #define RIGHT_ENC_PIN_B 19  //pin 19
+
+  RotaryEncoder encoderLeft(LEFT_ENC_PIN_A, LEFT_ENC_PIN_B); // Left encoder pins
+  RotaryEncoder encoderRight(RIGHT_ENC_PIN_A, RIGHT_ENC_PIN_B); // Right encoder pins
 #endif
-   
 long readEncoder(int i);
 void resetEncoder(int i);
 void resetEncoders();
